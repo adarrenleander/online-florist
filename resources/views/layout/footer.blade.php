343be21585360ebb82c30d6fa331c6b2cab67937
Amadeus Darren Leander <amadeusdarrenleander@gmail.com>
2019-10-11 23:37:21 +0700

    Created initial master template and Deleted Documents folder

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,4 @@
+<footer>
+    <p>&copy; Copyright 2019</p>
+    <p>All rights reserved. Created by <Span>ADLworks</Span></p>
+</footer>
---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,11 @@
+<header>
+    <div class="header-container">
+        <div class="header-left">
+            <a href="/">Online Florist</a>
+        </div>
+        <div class="header-right">
+            <a href="/login">Login</a>
+            <a href="/register">Register</a>
+        </div>
+    </div>
+</header>
