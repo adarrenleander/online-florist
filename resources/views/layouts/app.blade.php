e8879e48d7085ed77066193e31ec1b2246da3c13
Amadeus Darren Leander <amadeusdarrenleander@gmail.com>
2019-11-11 21:09:55 +0700

    Modified footer and welcome page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,13 +18,14 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+
+    <!-- Icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
     <div id="app">
         @include('layouts.header')
-        <main class="py-4">
-            @yield('content')
-        </main>
+        @yield('content')
         @include('layouts.footer')
     </div>
 </body>
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
 <footer>
-    <p>&copy; Copyright 2019</p>
-    <p>All rights reserved. Created by <Span>ADLworks</Span></p>
+    <h6>Made with <i class="fa fa-heart"></i> in Jakarta, Indonesia</h6>
+    <h6>Copyright 2019 | Created by <span>ADLworks</span></h6>
 </footer>
