38fe85f3bbe61d8a3790785509301590a7c41950
Amadeus Darren Leander <amadeusdarrenleander@gmail.com>
2019-11-11 20:38:56 +0700

    Developed new navigation header using Laravel Auth's header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,7 @@
-<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+<nav class="navbar navbar-expand-md navbar-light bg-red">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
-            {{ config('app.name', 'Laravel') }}
+            Online Florist
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
