80ebbdee5cb97f7493cfa73bc52d7e1f4d336a8e
Amadeus Darren Leander <amadeusdarrenleander@gmail.com>
2019-12-04 22:07:50 +0700

    Created 419 Page Expired view

---
+++ resources/views/errors/419.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('title', '419 Page Expired - Online Florist')
+
+@section('content')
+<div class="content errors">
+    <div class="content-container">
+        <h2>419 | Page Expired</h2>
+    </div>
+</div>
+@endsection
